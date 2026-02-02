4347bef240779fc66e34fa1f0a93e1551968d035
Muhittin Camdali <muhittincamdali@gmail.com>
2026-02-03 01:59:30 +0300

    feat: initial project structure

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ARCraft",
+    platforms: [
+        .iOS(.v16),
+        .visionOS(.v1),
+        .macOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "ARCraft",
+            targets: ["ARCraft"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "ARCraft",
+            dependencies: [],
+            path: "Sources/ARCraft",
+            swiftSettings: [
+                .define("ARCRAFT_VISIONOS", .when(platforms: [.visionOS])),
+                .define("ARCRAFT_IOS", .when(platforms: [.iOS]))
+            ]
+        ),
+        .testTarget(
+            name: "ARCraftTests",
+            dependencies: ["ARCraft"],
+            path: "Tests/ARCraftTests"
+        )
+    ]
+)
